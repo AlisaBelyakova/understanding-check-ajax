ec2035e677d803c54adc1f5d72b8692a6e43560e
AlisaBelyakova <6183112@gmail.com>
2018-03-31 22:02:01 -0400

    front end routes created

--- client/index.js
+++ client/index.js
@@ -2,7 +2,13 @@
 /*
 Your Tasks:
 
-1. If there is a front end route, render the appropriate data. We have routes on the backend that will send us array of data containing the appropriate cute animals! If there is a front end route, we should send a GET request using AJAX to request those data from the Server. Then we can call `renderView` to render the data to the page.
+1. If there is a front end route, render the appropriate data. 
+    We have routes on the backend that will send us array of data 
+    containing the appropriate cute animals! 
+
+If there is a front end route, we should send a GET request 
+    using AJAX to request those data from the Server. 
+    Then we can call `renderView` to render the data to the page.
    a) /#dogs, render all the dogs!
    b) /#cats, render all the cats!
    c) /#dragons, render all the dragons!
@@ -40,3 +46,52 @@ function renderView(arrOfData) {
   //append the new list to the content div
   view.appendChild(list);
 }
+
+import axios from 'axios';
+window.addEventListener('load', function (){
+  console.log('loaded')
+  
+  const dogs = axios('/dogs').then(res => res.data)
+  const cats = axios('/cats').then(res => res.data)
+  const drags = axios('/dragons').then(res => res.data)
+  
+  Promise.all([dogs,cats,drags])
+    .then(function (values){
+      switch (location.hash) {
+        case '#dogs':
+          renderView(values[0]);
+          break;
+        case '#cats':
+          renderView(values[1]);
+          break;
+        case '#dragons':
+          renderView(values[2]);
+          break;
+        default:
+          new Error('there is no such animal');
+          break;
+      }
+    })
+    
+  Promise.all([dogs,cats,drags])
+    .then(function(){
+      document.getElementById('dogs')
+        .addEventListener('click', function (event) {
+          location.hash = '#dogs';
+          renderView(dogs)
+        });
+      document.getElementById('cats')
+        .addEventListener('click', function (event) {
+          location.hash = '#cats';
+          renderView(cats)
+        });
+      document.getElementById('dragons')
+        .addEventListener('click', function (event) {
+          location.hash = '#dragons';
+          renderView(drags)
+        });
+    })
+
+})
+
+
